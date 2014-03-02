c2d9dc19b1372d547c24b1604a7f1bdb0cb6dae7
Rajesh Shenoy <shenoybr@Rajeshs-MacBook-Pro.local>
2014-03-02 01:53:19 -0500

    Added script to auto link

---
+++ link.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+####################################################
+#Conviniently setup symlinks for preferences files #
+####################################################
+
+ME=`basename $0`
+
+for FILE in `ls -A ~/dotfiles`;
+do
+    if [[ $FILE != $ME &&  ! $FILE =~ .git* 
+                && ! $FILE =~ .*.swp && ! -d $FILE ]];then
+        echo "Linking ${FILE}"
+        if [ -e ~/$FILE ];then
+            if [ -h ~/$FILE ];then
+                rm ~/$FILE
+            else
+                mkdir -p ~/bk
+                mv ~/${FILE} ~/bk/${FILE}.bk.`date +%m%d%Y`
+            fi
+        fi
+        ln -s ~/dotfiles/${FILE} $FILE
+    fi
+done
+
